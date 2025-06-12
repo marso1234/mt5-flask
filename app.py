a358f67af5a98bafc83ac4df03121961812e82ed
So Ho Mang Marcus <107460995+marso1234@users.noreply.github.com>
2025-06-12 10:57:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,14 +100,14 @@ def start_order():
         "symbol": symbol,
         "volume": size,
         "type": order_type_enum,
-        "price": price,
+        "price": float(price),
         "deviation": 10,
         "magic": 234000,
         "comment": order_id_gen,
         "type_time": mt5.ORDER_TIME_GTC,
         "type_filling": mt5.ORDER_FILLING_FOK,
-        'sl': stop_loss,
-        'tp': stop_profit
+        'sl': float(stop_loss),
+        'tp': float(stop_profit)
     }
 
     print(request_data)
